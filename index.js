a3a1fa2b6023dba61fb1589986aa182e99d78789
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-03-22 14:07:08 +0530

    add getbalance function to check balance
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { ethers } from "./ethers-6.7.0-min.js";
 import { abi, contractAddress } from "./constants.js";
 
-const connectButton = document.getElementById("connectButton");
-const fundButton = document.getElementById("fundButton");
+const connectButton = document.querySelector("#connectButton");
+const fundButton = document.querySelector("#fundButton");
+const balanceButton = document.querySelector("#balanceButton");
 connectButton.onclick = connectWallet;
 fundButton.onclick = fund;
+balanceButton.onclick = getBalance;
 
 async function connectWallet() {
   if (typeof window.ethereum === "undefined") {
@@ -23,6 +25,17 @@ async function connectWallet() {
     connectButton.innerText = "Failed to connect";
     return false;
   }
+}
+
+async function getBalance() {
+  if (typeof window.ethereum === "undefined") {
+    console.log("Metamask is not installed.");
+    return;
+  }
+  const provider = new ethers.BrowserProvider(window.ethereum);
+
+  const balance = await provider.getBalance(contractAddress);
+  console.log(`Balance: ${ethers.formatEther(balance)} ETH`);
 }
 
 // fund function
@@ -59,6 +72,7 @@ async function fund() {
 async function listenForTransactionMine(transactionResponse, provider) {
   console.log(`Mining ${transactionResponse.hash}...`);
 
+  // wait for transaction to finish
   return new Promise(async (resolve, reject) => {
     try {
       const receipt = await provider.waitForTransaction(
@@ -67,8 +81,10 @@ async function listenForTransactionMine(transactionResponse, provider) {
       const confirmations =
         (await provider.getBlockNumber()) - receipt.blockNumber + 1;
       console.log(`Completed with ${confirmations} confirmations`);
+      // Got enough confirmations
       resolve();
     } catch (error) {
+      // Weird, let's log the error
       console.log("Error while waiting for transaction confirmation:", error);
       reject(error);
     }
